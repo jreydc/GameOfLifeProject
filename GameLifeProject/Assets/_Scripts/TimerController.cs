d763a6b00305cb29835f23218ec7936336d098db
jreydc <jreydc@gmail.com>
2022-06-12 17:57:48 +0800

    Create TimerController.cs

---
+++ GameLifeProject/Assets/_Scripts/TimerController.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Timer", fileName = "TimerController")]
+public class TimerController : ScriptableObject
+{
+    public Timer timerInstance;
+}
